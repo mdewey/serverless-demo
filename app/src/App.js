047ea34c7fef7f1055a56b5c5133422418989d80
Mark Dewey <mark.t.dewey@gmail.com>
2018-02-10 21:02:38 -0500

    added basic content for the home page, ready to implement Auth0

--- app/src/App.js
+++ app/src/App.js
@@ -1,45 +1,20 @@
+import { Route } from 'react-router-dom';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Home from './pages/Home.jsx'
 
 class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      secret: "loading..."
-    }
-  }
-
-  componentDidMount() {
-    fetch("https://40bro24ihl.execute-api.us-east-1.amazonaws.com/dev")
-      .then(resp => {
-        console.log(resp)
-        return resp.json()
-      })
-      .then(data => {
-        console.log(data);
-        this.setState(() => {
-          return {
-            secret: data.hello
-          }
-        })
-      })
-  }
-
+  constructor() {
+    super();
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {this.state.secret}
-        </p>
+      <div>
+        <div className="App">
+          <Route path="/" exact component={Home}></Route>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
